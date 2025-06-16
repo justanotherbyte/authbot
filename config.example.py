d603daca54d5d1fac0190b0eadae725f8675dd6b
justanotherbyte <wishymovies@gmail.com>
2025-06-16 23:52:35 +0100

    documentation

--- config.example.py
+++ config.example.py
@@ -1,32 +1,32 @@
 # Discord
-TOKEN = "<YOUR DISCORD TOKEN HERE>"
+TOKEN = "<YOUR DISCORD TOKEN HERE>"  # Your Discord Bot Token
 
 # Gmail
-EMAIL_USERNAME = "youremail@gmail.com"
-EMAIL_PASSWORD = "Gmail App Password"
-EMAIL_HOSTNAME = "smtp.gmail.com"
+EMAIL_USERNAME = (
+    "youremail@gmail.com"  # The email you'd like to send the code emails from.
+)
+EMAIL_PASSWORD = "Gmail App Password"  # The password to use to authenticate with the SMTP server. If this is Gmail, enable 2FA and use an App Password.
+EMAIL_HOSTNAME = "smtp.gmail.com"  # Email host domain
 EMAIL_PORT = 587
 
 # PostgreSQL
-DATABASE_URL = "postgresql://postgres:authbot@localhost:5432/postgres"
+DATABASE_URL = "postgresql://[username]:[password]@[host]:[port]/[db name]"  # postgres connection uri
 
 # Functionality
 ## Emails
 WELCOME_MESSAGE = (
     "Hi. $MENTION I'm AuthBot. Welcome to $GUILD_NAME. "
     "Please use the below buttons to authenticate yourself."
-)
+)  # this is sent to the user directly in private messages when the user joins your guild.
+
 INSTITUTE_NAME = "University Name"
-EMAIL_SUBJECT = "Welcome to the University's Discord Eco-system"
+EMAIL_SUBJECT = "Welcome to the University's Discord Eco-system"  # the subject of the auth code email.
 
 ## Codes
-CODE_SIZE = 5
-CODE_POOL = "0123456789abcdefghijklmopqrstuvwxyz"
+CODE_SIZE = 5  # the size of codes. 5 is a pretty good number. There are 52521875 possible codes.
+CODE_POOL = "0123456789abcdefghijklmopqrstuvwxyz"  # the character pool that codes are generated from.
 EMAIL_HTML_TEMPLATE = "templates/email.html"
 EMAIL_TEXT_TEMPLATE = "templates/email.txt"
-
-## Discord
-AUTHENTICATED_ROLE_ID = 123456789
 
 
 def CHECK_EMAIL(email: str) -> bool:
