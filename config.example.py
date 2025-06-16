f15e55a3934501ae41b8f59135427263e04a3ad7
justanotherbyte <wishymovies@gmail.com>
2025-06-16 23:10:10 +0100

    add example configuration

---
+++ config.example.py
@@ -0,0 +1,33 @@
+# Discord
+TOKEN = "<YOUR DISCORD TOKEN HERE>"
+
+# Gmail
+EMAIL_USERNAME = ""
+EMAIL_PASSWORD = ""
+EMAIL_HOSTNAME = ""
+EMAIL_PORT = 587
+
+# PostgreSQL
+DATABASE_URL = "postgresql://postgres:authbot@localhost:5432/postgres"
+
+# Functionality
+## Emails
+WELCOME_MESSAGE = (
+    "Hi. $MENTION I'm AuthBot. Welcome to $GUILD_NAME. "
+    "Please use the below buttons to authenticate yourself."
+)
+INSTITUTE_NAME = "University Name"
+EMAIL_SUBJECT = "Welcome to the University's Discord Eco-system"
+
+## Codes
+CODE_SIZE = 5
+CODE_POOL = "0123456789abcdefghijklmopqrstuvwxyz"
+EMAIL_HTML_TEMPLATE = "templates/email.html"
+EMAIL_TEXT_TEMPLATE = "templates/email.txt"
+
+## Discord
+AUTHENTICATED_ROLE_ID = 123456789
+
+
+def CHECK_EMAIL(email: str) -> bool:
+    return email.endswith(".ed.ac.uk")
